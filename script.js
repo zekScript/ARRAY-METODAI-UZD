6ed4993e4f47ade808537bedcfec2d865f26fc35
zekScript <armandas.latanauskas06@gmail.com>
2024-12-19 09:24:48 +0200

    Trecias uzd PADARYTAS

--- script.js
+++ script.js
@@ -1,25 +1,43 @@
+// Make an object that has objects of computer RAM CPU and maker of the computer and try to output the object of preferred method
 
-
-
-
-// Make a for cycle that outputs from 0 to 20 even numbers
-
-// for (let i = 0; i <= 20; i += 2) {
-
-//   console.log(i);
-
+const computers = [
+      {
+      //   Get specifications of the current object values
+        manufacturer: "Dell",
+        processor: "Intel Core i7",
+        RAM: "16GB",
+        getSpecifications() {
+          return `Manufacturer: ${this.manufacturer}, Processor: ${this.processor}, RAM: ${this.RAM}`;
+        }
+      },
+      {
+        manufacturer: "Apple",
+        processor: "M1",
+        RAM: "8GB",
+        getSpecifications() {
+          return `Manufacturer: ${this.manufacturer}, Processor: ${this.processor}, RAM: ${this.RAM}`;
+        }
+      },
+      {
+        manufacturer: "HP",
+        processor: "AMD Ryzen 5",
+        RAM: "12GB",
+        getSpecifications() {
+          return `Manufacturer: ${this.manufacturer}, Processor: ${this.processor}, RAM: ${this.RAM}`;
+        }
+      }
+    ];
+//     Loop trough all objects available
+    computers.forEach((computer, index) => {
+      console.log(`Computer ${index + 1}: ${computer.getSpecifications()}`);
+    });
+    
+// const getComputerObj = () => {
+//       return `${computer.maker.computerMaker}'s computer with ${computer.CPU.model} CPU with ${computer.CPU.cores} cores ${computer.CPU.threads} threads and ${computer.RAM.capacity} RAM`;
 // }
 
-
-// Make a function that returns an element in right spot from the array (Aš nesuprantu į šitą užd bet parašiau kaip suprantu)
-
-const arrOfNum = [20, 10, 0, 5, 1, 40, 26, 2];
+// console.log(getComputerObj()); 
 
 
-function findRightSpot(arr, num) {
-      return arr[num]
-}
-
-console.log(findRightSpot(arrOfNum, 1));
 
 
